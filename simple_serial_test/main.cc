ae42a71feeefc2e7234bafa1f2b767a20b532eed
tsullins92 <tsullins92@gmail.com>
2017-06-15 20:31:12 -0500

    first commit

---
+++ simple_serial_test/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../serial-port/1_simple/SimpleSerial.h"
+
+using namespace std;
+using namespace boost;
+
+int main(int argc, char* argv[])
+{
+    try {
+
+        SimpleSerial serial("/dev/ttyUSB0",115200);
+
+        serial.writeString("Hello world\n");
+
+        cout<<serial.readLine()<<endl;
+
+    } catch(boost::system::system_error& e)
+    {
+        cout<<"Error: "<<e.what()<<endl;
+        return 1;
+    }
+}
